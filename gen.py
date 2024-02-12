989d77b120ad064bb844689c272d8da7f5c86de1
Nicolas signed-log FORMICHELLA <stigpro@outlook.fr>
2024-02-12 08:14:31 +0100

    Working scrape

--- gen.py
+++ gen.py
@@ -1,27 +1,24 @@
 #!/usr/bin/env python3
 
 from jinja2 import Environment, PackageLoader, select_autoescape
-env = Environment(
-    loader=PackageLoader("gen"),
-    autoescape=select_autoescape()
-)
-template = env.get_template("app-helm.yaml.j2")
+
+env = Environment(loader=PackageLoader("gen"), autoescape=select_autoescape())
+template = env.get_template("app-template.yaml.j2")
 
 
 env = {
-  "dev": {
-    "queues": ["rubis", "emeraude", "saphir", "diamant"]
-  },
-  "prod": {
-    "queues": ["rubis", "emeraude", "saphir", "diamant"]
-  },
-  "test": {
-    "queues": ["rubis", "emeraude", "saphir", "diamant"]
-  }
+  "dev": {"queues": ["rubis", "emeraude", "saphir", "diamant"]},
+  "prod": {"queues": ["rubis", "emeraude", "saphir", "diamant"]},
+  "test": {"queues": ["rubis", "emeraude", "saphir", "diamant"]},
 }
 
 
 for namespace in env:
   with open(f"deploy-{namespace}.yaml", "wt") as f:
-    f.write(template.render(env={namespace: env[namespace]}, image="ghcr.io/abacf/sae503:1.1.0", base_domain="172.18.200.33.nip.io",))
-    
+    f.write(
+      template.render(
+        env={namespace: env[namespace]},
+        image="ghcr.io/abacf/sae503:2.0.3",
+        base_domain="192.168.1.52.nip.io",
+      )
+    )
